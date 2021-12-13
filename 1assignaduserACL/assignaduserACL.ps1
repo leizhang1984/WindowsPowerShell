69f8a70f03238eb6dd5fbc035b34f2b3e1ea4648
Lei Zhang <kinkikid2005@hotmail.com>
2021-12-13 12:44:13 +0800

    2021-12-13

---
+++ 1assignaduserACL/assignaduserACL.ps1
@@ -0,0 +1,50 @@
+#指定csv文件路径，csv文件第一列列名必须为username
+$csvpath = "D:\WVD\users.csv"
+
+#需要创建文件夹的路径
+$folderpath = "D:\WVD\"
+
+#Start to Process CSV File
+$p = Import-Csv -Path $csvpath
+
+foreach ($rows in $p)
+{
+        try
+        {
+               #如果文件夹路径最后带斜杠("\")，则去除
+               if($folderpath.EndsWith("\"))
+               {
+                   $folderpath = $folderpath.Substring(0,$folderpath.Length-1)
+               }
+               
+               #creat folder
+               $username = $rows.username
+               $newuserfolder = $folderpath + "\" + $username
+               New-Item -Path $newuserfolder -ItemType directory 
+
+               #SET ACL
+               #Domain Name
+               $domain = "FAREAST"
+
+               #user id
+               $userid = $domain + "\" + $username
+
+               $inheritanceFlag = [System.Security.AccessControl.InheritanceFlags]"ContainerInherit, ObjectInherit"
+               $propagationFlag = [System.Security.AccessControl.PropagationFlags]::None
+               $type = [System.Security.AccessControl.AccessControlType]::Allow
+
+               $ace = New-Object System.Security.AccessControl.FileSystemAccessRule($userid,"FullControl",$inheritanceFlag, $propagationFlag,$type)
+
+               $ACL.AddAccessRule($ace)
+               $acl = Get-Acl $newuserfolder
+               $acl.SetAccessRule($ace)
+               Set-Acl $newuserfolder $acl
+        }
+        Catch
+        {
+                Write-Error "Processing with a Error, Resume Next"
+        }
+}
+
+
+Write-Output "Processing Finish"
